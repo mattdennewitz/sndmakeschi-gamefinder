ecb5a8f27be5aea24b4bbfe8a77e45b32df21c43
Matt Dennewitz <mattdennewitz@gmail.com>
2016-02-07 14:08:55 -0600

    :tv: youtube url interp

--- src/components/game-detail.js
+++ src/components/game-detail.js
@@ -2,14 +2,13 @@
 
 export default (props) => {
   const { game } = props;
-
-  console.log(game);
+  const youtubeUrl = 'http://www.youtube.com/embed/' + game.youtubeId;
 
   return (
     <div className="py2">
       <h1 className="center">{game.name}</h1>
       <div className="embed-container">
-        <iframe src='http://www.youtube.com/embed/{game.youtubeId}' frameborder='0' allowfullscreen></iframe>
+        <iframe src={youtubeUrl} frameborder='0' allowfullscreen></iframe>
       </div>
       <div className="px3">
         <article className="py2"><p>{game.description}</p></article>
